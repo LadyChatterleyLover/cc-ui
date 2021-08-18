7c948a98fe04109ea39e060418909043ba3cddce
五月的夏天 <285258675@qq.com>
2021-08-18 18:41:21 +0800

    新增全屏滚动组件

---
+++ components/cc-full-tabs/cc-full-tabs.vue
@@ -0,0 +1,44 @@
+<template>
+  <view class="cc-full-tabs">
+    <cc-tabs :list="list" :current="current" @change="changeTabs"></cc-tabs>
+    <slot></slot>
+  </view>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    list: {
+      type: Array,
+      required: true
+    },
+    current: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    changeTabs(val) {
+      this.$emit('update:current', val)
+    }
+  },
+  mounted() {},
+  onLoad() {},
+  onShow() {},
+  filters: {},
+  computed: {},
+  watch: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.cc-full-tabs {
+}
+</style>
---
+++ views/fullTabs/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <view class="container">
+    <cc-full-tabs :list='list' :current.sync='current'>
+      <view class="content">
+        <swiper :indicator-dots="false" :autoplay="false" :current="current" @change="changeSwiper">
+          <swiper-item>
+            <view style="background: red;">1</view>
+          </swiper-item>
+          <swiper-item>
+            <view style="background: blue;">2</view>
+          </swiper-item>
+          <swiper-item>
+            <view style="background: green;">3</view>
+          </swiper-item>
+        </swiper>
+      </view>
+    </cc-full-tabs>
+  </view>
+
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+        list: [
+          {
+            title: '待收货'
+          },
+          {
+            title: '待付款'
+          },
+          {
+            title: '待评价',
+            badge: 5
+          }
+        ],
+        current: 0
+      }
+    },
+    methods: {
+      changeSwiper(e) {
+        this.current = e.detail.current
+      },
+    },
+    mounted() {
+
+    },
+    onLoad() {
+
+    },
+    onShow() {
+
+    },
+    filters: {},
+    computed: {},
+    watch: {},
+  }
+</script>
+
+<style scoped lang="scss">
+ .content {
+   height: calc(100vh - 35px);
+   swiper {
+     height: 100%;
+   }
+   view {
+     height: 100%;
+   }
+ }
+</style>
